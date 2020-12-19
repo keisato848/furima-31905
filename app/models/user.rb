4f3386a419730904ac6ece06bc893d21762b95f3
keisato848 <72418986+keisato848@users.noreply.github.com>
2020-12-19 18:24:46 +0900

    rubocopを通してコードを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        
- VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/.freeze
   validates :nickname, presence: true, uniqueness: true
-  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX}
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_kana, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,77 +11,77 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
       it 'passwordが半角英数字６文字以上であれば登録できる' do
-        @user.password = "test00"
-        @user.password_confirmation = "test00"
+        @user.password = 'test00'
+        @user.password_confirmation = 'test00'
         expect(@user).to be_valid
       end
     end
 
     context 'ユーザー新規登録がうまくいかないとき' do
       it 'nicknameが空だと登録できない' do
-        @user.nickname = "" 
+        @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
       it 'emailが空だと登録できない' do
-        @user.email = ""
+        @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
       it 'passwordが空だと登録できない' do
-        @user.password = ""
+        @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
       it 'last_nameが空だと登録できない' do
-        @user.last_name = ""
+        @user.last_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
       it 'first_nameが空だと登録できない' do
-        @user.first_name = ""
+        @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank")
       end
       it 'last_kanaが空だと登録できない' do
-        @user.last_kana = ""
+        @user.last_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last kana can't be blank")
       end
       it 'first_kanaが空だと登録できない' do
-        @user.first_kana = ""
+        @user.first_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First kana can't be blank")
       end
       it 'birth_dateが空だと登録できない' do
-        @user.birth_date = ""
+        @user.birth_date = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birth date can't be blank")
       end
       it 'passwordが6文字未満だと登録できない' do
-        @user.password = "test0"
-        @user.password_confirmation = "test0"
+        @user.password = 'test0'
+        @user.password_confirmation = 'test0'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
       it 'passwordが半角英数字混在でなければ登録できない' do
-        @user.password = "000000"
+        @user.password = '000000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
       it '重複したnicknameが存在する場合登録できない' do
         @user.save
         another_user = FactoryBot.build(:user)
         another_user.nickname = @user.nickname
         another_user.valid?
-        expect(another_user.errors.full_messages).to include("Nickname has already been taken")
+        expect(another_user.errors.full_messages).to include('Nickname has already been taken')
       end
       it '重複したemailが存在する場合登録できない' do
         @user.save
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
     end
   end
