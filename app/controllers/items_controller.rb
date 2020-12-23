5ae0178c77a95f065a7ebe2b67188b553e69ffae
keisato848 <72418986+keisato848@users.noreply.github.com>
2020-12-23 21:01:35 +0900

    コントローラー、ルーティング、ビューの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to '/'
+    else
+      render :new
+    end
   end
 
   private
 
-  def iteme_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :state_id, :shipping_fee_burden_id, :prefecture_id, :delivery_day_id, :item_fee).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,30 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :state
+    belongs_to :prefecture
+    belongs_to :shipping_fee_burden
+    belongs_to :delivery_day
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :item_fee
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :prefecture_id
+    validates :shipping_fee_burden_id
+    validates :delivery_day_id
+  end
+
+  validates :item_fee, :numericality => {
+    :greater_than_or_equal_to => 300,
+    :less_than_or_equal_to => 9999999,
+    :message => '価格は300円から9999999円の範囲で入力してください'
+  }
 end
