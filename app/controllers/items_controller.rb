3639f2ae32d4da0aac09c9cbcc84c675dc2fb79a
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-01-09 19:30:30 +0900

    updateアクション内の変数をインスタンスに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,11 +27,11 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
     else
-      render 'edit'
+      render :edit
     end
   end
 
