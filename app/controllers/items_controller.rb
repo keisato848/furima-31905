1198aed0e5b522fe25a22dad45211f71c5be2cb5
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-01-09 20:48:55 +0900

    rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
-  before_action :find_item, only: [:show, :edit, :update, :destroy] 
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
   before_action :redirect_root, only: [:edit, :update, :destroy]
-  
+
   def index
     @items = Item.order(id: 'DESC')
   end
