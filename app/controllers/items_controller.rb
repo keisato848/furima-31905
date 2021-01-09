c3a9d3664ba514db8b61e1f675047706c14b63e7
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-01-09 20:34:20 +0900

    コントローラーとルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
-  before_action :find_item, only: [:show, :edit, :update] 
+  before_action :find_item, only: [:show, :edit, :update, :destroy] 
   
   def index
     @items = Item.order(id: 'DESC')
@@ -32,6 +32,9 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
   end
 
   private
