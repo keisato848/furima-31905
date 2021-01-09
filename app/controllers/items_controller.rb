68b018cb6a39780634f13e88569fd043ad2b8499
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-01-09 20:21:02 +0900

    authenticate_user!にupdateアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
   before_action :find_item, only: [:show, :edit, :update] 
   
   def index
