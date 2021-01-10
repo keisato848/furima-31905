3964c25061e4799648b1bbd8934dd1fea91c61cf
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-01-10 22:01:40 +0900

    購入画面へのルーティング、コントローラーの設定、ビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   root 'items#index'
-  resources :items
+  resources :items do
+    resources :orders, only: :index
+  end
 end
