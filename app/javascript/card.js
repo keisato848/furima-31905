677c95645a752656f1b53bf8e2072ed592a8cd72
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-01-11 21:03:09 +0900

    PAY.JP導入のためのフロントエンド実装

---
+++ app/javascript/card.js
@@ -0,0 +1,34 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_d5bad7d12d59a5a6936b3f94");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_address[number]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: formData.get("order_address[exp_year]"),
+      cvc: formData.get("order_address[cvc]"),
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforernd", tokenObj);
+      }
+
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+
+      document.getElementById("charge-form").onsubmit();
+    });
+  });
+};
+window.addEventListener("load", pay);
