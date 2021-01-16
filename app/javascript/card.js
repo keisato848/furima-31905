8d30c8b50beb1bfa1e49bc70867334c82a1d8d4a
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-01-16 12:42:03 +0900

    PAYJPを用いたカード決済の実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -2,7 +2,7 @@ const pay = () => {
   Payjp.setPublicKey("pk_test_d5bad7d12d59a5a6936b3f94");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
@@ -10,25 +10,26 @@ const pay = () => {
     const card = {
       number: formData.get("order_address[number]"),
       exp_month: formData.get("order_address[exp_month]"),
-      exp_year: formData.get("order_address[exp_year]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
       cvc: formData.get("order_address[cvc]"),
     };
-
+    
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforernd", tokenObj);
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
 
       document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-exp-month").removeAttribute("name");
-      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
   });
 };
+
 window.addEventListener("load", pay);
--- app/javascript/packs/new.js
+++ app/javascript/packs/new.js
@@ -4,10 +4,10 @@ window.addEventListener("load",()=>{
 
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price")
+    const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-     const profitNumber = document.getElementById("profit")
-     const value_result = inputValue * 0.1
+     const profitNumber = document.getElementById("profit");
+     const value_result = inputValue * 0.1;
      profitNumber.innerHTML = (Math.floor(inputValue - value_result));
-  })
-})
+  });
+});
