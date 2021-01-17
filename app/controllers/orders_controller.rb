3bd229faac4311a017bf6318a9e2df1604926277
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-01-17 22:23:41 +0900

    ユーザーログイン状態、商品の出品状態に合わせ、アクセスを制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_item
+  before_action :redirect_root
+  before_action :expect_seller
+  before_action :expect_sold_out
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
   
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
@@ -23,6 +27,23 @@ class OrdersController < ApplicationController
 
 
   private
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_root
+    redirect_to root_path unless user_signed_in?
+  end
+
+  def expect_seller
+    find_item
+    redirect_to root_path unless current_user.id =! @item.user_id
+  end
+
+  def expect_sold_out
+    find_item
+    redirect_to root_path if Order.find_by(item_id: @item.id)
+  end
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :telephone_number).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
